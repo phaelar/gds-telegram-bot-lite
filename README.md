551cdaf32e820dc72f0077e2ebea3fbc7d8fec7c
Raphael Tan <raphaeltan@Raphaels-MBP.lan>
2015-10-22 13:54:03 +0800

    - Add functionality to add new quotes through the bot - Add new quotes - Updated README

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
+# GDS-telegram-bot-lite
+
 Simple Telegram bot for IDA GDS!
 Still work in progress!
+
+Using the  [telegram-bot-ruby](https://github.com/atipugin/telegram-bot-ruby) wrapper for Telegram API.
+
+## Installation
+Execute:
+
+```shell
+$ bundle
+```
+
+## Running the bot
+
+First things first, you need to [obtain a token](https://core.telegram.org/bots#botfather) for your bot. Then add your API token as an environment variable 'API_TOKEN'
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch (git checkout -b my-new-feature)
+3. Commit your changes (git commit -am 'Add some feature')
+4. Push to the branch (git push origin my-new-feature)
+5. Create new Pull Request
+
+## Usage
+
+`/qotd`: Return a random quote
+
+`/qotd_add <author name> the actual quote`: Add your own quote to the list!
--- bot.rb
+++ bot.rb
@@ -7,7 +7,14 @@ token = ENV['API_TOKEN']
 $h = {}
 
 class MessageParser
-  @@quotes = YAML.load_file('quotes.yml')
+  @@quote_file = 'quotes.yml'
+  @@quotes = YAML.load_file(@@quote_file)
+
+  def self.rewrite_quotes_file
+    File.open(@@quote_file, 'w') do |h|
+      h.write @@quotes.to_yaml
+    end
+  end
 
   def self.get_random_quote
     author = @@quotes.keys.sample
@@ -15,17 +22,45 @@ class MessageParser
     "#{author}: \n #{statement}"
   end
 
+  def self.add_to_quotes(author, phrase)
+    @@quotes[author] = [] unless @@quotes.keys.include? author
+    @@quotes[author] << phrase
+    self.rewrite_quotes_file
+  end
+
+  def self.handle_quotes(bot,message)
+    p 'parsing quotes'
+
+    message_arr = message.text.split
+    case message_arr[0]
+    when "/qotd"
+      bot.api.send_message(chat_id: message.chat.id, text: self.get_random_quote)
+    when "/qotd_add"
+      message.text.slice! "/qotd_add"
+      author_raw = /<.+>/.match(message.text).to_s
+      message.text.slice! author_raw.to_s
+      message.text.slice " "
+
+      author = author_raw[1..-2]
+      phrase = message.text[1..-1]
+      self.add_to_quotes(author, phrase)
+      bot.api.send_message(chat_id: message.chat.id, text: "Quote added! \n#{author}: #{message}")
+    end
+  end
+
   def self.parse_commands(bot, message)
     p 'parsing commands'
+
     case message.text
-    when '/start'
+    when /^\/start/
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}!")
-    when '/end'
+    when /^\/end/
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}!")
-    when '/qotd'
-      bot.api.send_message(chat_id: message.chat.id, text: get_random_quote)
+    when /^\/qotd/
+      # bot.api.send_message(chat_id: message.chat.id, text: get_random_quote)
+      handle_quotes(bot,message)
     else
-      bot.api.send_message(chat_id: message.chat.id, text: "I'm sorry Dave, I'm afraid I can't do that.")
+      bot.api.send_message(chat_id: message.chat.id, text: "I'm sorry #{message.from.first_name}, I'm afraid I can't do that.")
     end
   end
 
