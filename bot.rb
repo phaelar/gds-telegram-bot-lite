6e0ecbfc0dd4532ed3970fff15c7c27296becd51
Raphael Tan <raphaeltanyw@gmail.com>
2016-02-11 10:17:24 +0800

    Add 502 error handling

--- bot.rb
+++ bot.rb
@@ -15,12 +15,18 @@ $bot_name = ENV['TELEGRAM_BOT_NAME']
 p "Starting bot..."
 
 Telegram::Bot::Client.run(token) do |bot|
-  bot.listen do |message|
-    next if message.text.nil?
-    if message.text.start_with?('/')
-      MessageParser.parse_commands(bot, message)
-    else
-      MessageParser.parse_plaintext(bot, message)
+  begin
+    bot.listen do |message|
+      next if message.text.nil?
+      if message.text.start_with?('/')
+        MessageParser.parse_commands(bot, message)
+      else
+        MessageParser.parse_plaintext(bot, message)
+      end
+    end
+  rescue Telegram::Bot::Exceptions::ResponseError => e
+    if e.error_code.to_s == '502'
+      puts 'Telegram 502 error'
     end
   end
 end
